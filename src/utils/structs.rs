ae269f598f1cf30d53d806318850b4f9b43094ba
jmmonteiro <>
2022-12-01 17:20:01 +0000

    Refactor

--- src/utils/structs.rs
+++ src/utils/structs.rs
@@ -1,24 +1,22 @@
 use std::time::Instant;
 pub struct Answer<'a> {
-	pub prob_number: &'a str,
-	pub answer: String,
-	pub time: Instant,
+    pub prob_number: &'a str,
+    pub answer: String,
+    pub time: String,
 }
 
 impl<'a> Answer<'a> {
-	pub fn new(prob_number: &'a str, answer: String, time: Instant) -> Answer {
-		Answer {
-			prob_number,
-			answer,
-			time,
-		}
-	}
-	pub fn display(&self) {
-		println!(
-			"(runtime: {} ms)\t Problem {}:\t {}",
-			self.time.elapsed().as_millis().to_string(),
-			self.prob_number,
-			self.answer,
-		)
-	}
+    pub fn new(prob_number: &'a str, answer: String, time: Instant) -> Answer {
+        Answer {
+            prob_number,
+            answer,
+            time: time.elapsed().as_millis().to_string(),
+        }
+    }
+    pub fn display(&self) {
+        println!(
+            "(runtime: {} ms)\t Problem {}:\t {}",
+            self.time, self.prob_number, self.answer,
+        )
+    }
 }
