ec29c964a6e04000674a3f154333ae05958eef03
jmmonteiro <>
2022-12-05 21:43:39 +0000

    Added total time

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 mod days;
 mod utils;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use crate::utils::structs::Solver;
 fn main() {
-    let mut total_time = Duration::new(0, 0);
+    let time = Instant::now();
+    let mut total_algo_time = Duration::new(0, 0);
 
     println!("");
-    total_time += days::day01::Day.solve("inputs/day01.txt");
-    total_time += days::day02::Day.solve("inputs/day02.txt");
-    total_time += days::day03::Day.solve("inputs/day03.txt");
-    total_time += days::day04::Day.solve("inputs/day04.txt");
-    total_time += days::day05::Day.solve("inputs/day05.txt");
+    total_algo_time += days::day01::Day.solve("inputs/day01.txt");
+    total_algo_time += days::day02::Day.solve("inputs/day02.txt");
+    total_algo_time += days::day03::Day.solve("inputs/day03.txt");
+    total_algo_time += days::day04::Day.solve("inputs/day04.txt");
+    total_algo_time += days::day05::Day.solve("inputs/day05.txt");
 
     println!(
-        "Total algorithm solve time (excludes data reading time): {} s\n",
-        (total_time.as_nanos() as f32) / (i32::pow(10, 9) as f32)
+        "Total algorithm solve time (excludes data reading time): {} s",
+        (total_algo_time.as_nanos() as f32) / (i32::pow(10, 9) as f32)
+    );
+    println!(
+        "Total runtime: {} s\n",
+        (time.elapsed().as_nanos() as f32) / (i32::pow(10, 9) as f32)
     );
 }
