ce2754b19bd7cf71d21587f5cafcc5d9527d3a2c
jmmonteiro <>
2022-12-06 16:04:14 +0000

    Added benchmarks for all

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Advent of Code 2022
 
 My attempt to improve my Rust skills by solving the 2022 Advent of Code puzzles.
+
+- To get all the solutions: `cargo run --release`
+- To run the unit tests: `cargo test`
+- To run the benchmarks: `cargo bench`
--- benches/aoc_benchmarks.rs
+++ benches/aoc_benchmarks.rs
@@ -3,7 +3,7 @@ use aoc2022::{utils::input::read_file, utils::structs::Solver};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-pub fn day01_benchmark(c: &mut Criterion) {
+pub fn benchmarks(c: &mut Criterion) {
     let vec = read_file("inputs/day01.txt");
     c.bench_function("day01_part1", |b| {
         b.iter(|| day01::Day.part1(black_box(&vec)))
@@ -11,6 +11,46 @@ pub fn day01_benchmark(c: &mut Criterion) {
     c.bench_function("day01_part2", |b| {
         b.iter(|| day01::Day.part2(black_box(&vec)))
     });
+
+    let vec = read_file("inputs/day02.txt");
+    c.bench_function("day02_part1", |b| {
+        b.iter(|| day02::Day.part1(black_box(&vec)))
+    });
+    c.bench_function("day02_part2", |b| {
+        b.iter(|| day02::Day.part2(black_box(&vec)))
+    });
+
+    let vec = read_file("inputs/day03.txt");
+    c.bench_function("day03_part1", |b| {
+        b.iter(|| day03::Day.part1(black_box(&vec)))
+    });
+    c.bench_function("day03_part2", |b| {
+        b.iter(|| day03::Day.part2(black_box(&vec)))
+    });
+
+    let vec = read_file("inputs/day04.txt");
+    c.bench_function("day04_part1", |b| {
+        b.iter(|| day04::Day.part1(black_box(&vec)))
+    });
+    c.bench_function("day04_part2", |b| {
+        b.iter(|| day04::Day.part2(black_box(&vec)))
+    });
+
+    let vec = read_file("inputs/day05.txt");
+    c.bench_function("day05_part1", |b| {
+        b.iter(|| day05::Day.part1(black_box(&vec)))
+    });
+    c.bench_function("day05_part2", |b| {
+        b.iter(|| day05::Day.part2(black_box(&vec)))
+    });
+
+    let vec = read_file("inputs/day06.txt");
+    c.bench_function("day06_part1", |b| {
+        b.iter(|| day06::Day.part1(black_box(&vec)))
+    });
+    c.bench_function("day06_part2", |b| {
+        b.iter(|| day06::Day.part2(black_box(&vec)))
+    });
 }
-criterion_group!(benches, day01_benchmark);
+criterion_group!(benches, benchmarks);
 criterion_main!(benches);
