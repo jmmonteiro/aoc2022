32367d6123f8e8c1cc922a548570c03bcc671d9e
jmmonteiro <>
2022-12-09 16:09:15 +0000

    Refactored benchmarks

--- benches/aoc_benchmarks.rs
+++ benches/aoc_benchmarks.rs
@@ -3,78 +3,85 @@ use aoc2022::{utils::input::read_file, utils::structs::Solver};
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-pub fn benchmarks(c: &mut Criterion) {
+pub fn all_days_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("All Days");
+
     let vec = read_file("inputs/day01.txt");
-    c.bench_function("day01_part1", |b| {
+    group.bench_function("Day 1 : Part 1", |b| {
         b.iter(|| day01::Day.part1(black_box(&vec)))
     });
-    c.bench_function("day01_part2", |b| {
+    group.bench_function("Day 1 : Part 2", |b| {
         b.iter(|| day01::Day.part2(black_box(&vec)))
     });
 
     let vec = read_file("inputs/day02.txt");
-    c.bench_function("day02_part1", |b| {
+    group.bench_function("Day 2 : Part 1", |b| {
         b.iter(|| day02::Day.part1(black_box(&vec)))
     });
-    c.bench_function("day02_part2", |b| {
+    group.bench_function("Day 2 : Part 2", |b| {
         b.iter(|| day02::Day.part2(black_box(&vec)))
     });
 
     let vec = read_file("inputs/day03.txt");
-    c.bench_function("day03_part1", |b| {
+    group.bench_function("Day 3 : Part 1", |b| {
         b.iter(|| day03::Day.part1(black_box(&vec)))
     });
-    c.bench_function("day03_part2", |b| {
+    group.bench_function("Day 3 : Part 2", |b| {
         b.iter(|| day03::Day.part2(black_box(&vec)))
     });
 
     let vec = read_file("inputs/day04.txt");
-    c.bench_function("day04_part1", |b| {
+    group.bench_function("Day 4 : Part 1", |b| {
         b.iter(|| day04::Day.part1(black_box(&vec)))
     });
-    c.bench_function("day04_part2", |b| {
+    group.bench_function("Day 4 : Part 2", |b| {
         b.iter(|| day04::Day.part2(black_box(&vec)))
     });
 
     let vec = read_file("inputs/day05.txt");
-    c.bench_function("day05_part1", |b| {
+    group.bench_function("Day 5 : Part 1", |b| {
         b.iter(|| day05::Day.part1(black_box(&vec)))
     });
-    c.bench_function("day05_part2", |b| {
+    group.bench_function("Day 5 : Part 2", |b| {
         b.iter(|| day05::Day.part2(black_box(&vec)))
     });
 
     let vec = read_file("inputs/day06.txt");
-    c.bench_function("day06_part1", |b| {
+    group.bench_function("Day 6 : Part 1", |b| {
         b.iter(|| day06::Day.part1(black_box(&vec)))
     });
-    c.bench_function("day06_part2", |b| {
+    group.bench_function("Day 6 : Part 2", |b| {
         b.iter(|| day06::Day.part2(black_box(&vec)))
     });
 
     let vec = read_file("inputs/day07.txt");
-    c.bench_function("day07_part1", |b| {
+    group.bench_function("Day 7 : Part 1", |b| {
         b.iter(|| day07::Day.part1(black_box(&vec)))
     });
-    c.bench_function("day07_part2", |b| {
+    group.bench_function("Day 7 : Part 2", |b| {
         b.iter(|| day07::Day.part2(black_box(&vec)))
     });
 
     let vec = read_file("inputs/day08.txt");
-    c.bench_function("day08_part1", |b| {
+    group.bench_function("Day 8 : Part 1", |b| {
         b.iter(|| day08::Day.part1(black_box(&vec)))
     });
-    c.bench_function("day08_part2", |b| {
+    group.bench_function("Day 8 : Part 2", |b| {
         b.iter(|| day08::Day.part2(black_box(&vec)))
     });
 
     let vec = read_file("inputs/day09.txt");
-    c.bench_function("day09_part1", |b| {
+    group.bench_function("Day 9 : Part 1", |b| {
         b.iter(|| day09::Day.part1(black_box(&vec)))
     });
-    c.bench_function("day09_part2", |b| {
+    group.bench_function("Day 9 : Part 2", |b| {
         b.iter(|| day09::Day.part2(black_box(&vec)))
     });
 }
-criterion_group!(benches, benchmarks);
-criterion_main!(benches);
+criterion_group! {
+    name = all_days;
+    config = Criterion::default().significance_level(0.01).sample_size(500);
+    targets = all_days_benchmark
+}
+
+criterion_main!(all_days);
